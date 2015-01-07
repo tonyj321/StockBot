a382ca9ea4d29ca35fc7391497cfa9e26b0acbc3
Arun <Arun@10.0.0.144>
2015-01-06 20:56:32 -0800

    Added loop so MACD can give indicators for a year, and calculate given profit.

--- src/main/java/com/aruninc/stockbot/StockBot.java
+++ src/main/java/com/aruninc/stockbot/StockBot.java
@@ -20,21 +20,59 @@ public class StockBot {
 
     public static void main(String[] args) throws IOException {
         StockBot stockBot = new StockBot();
-        stockBot.readZip("/home/tonyj/Downloads/NASDAQ_2014.zip");
-        final Stock stock = stockBot.stockMap.get("IRBT");
+        stockBot.readZip("C:\\Users\\Arun\\StockBot\\NASDAQ_2014.zip");
+        System.out.println("Nasdaq 2014 archived");
+        stockBot.readZip("C:\\Users\\Arun\\StockBot\\NASDAQ_2013.zip");
+        System.out.println("Nasdaq 2013 archived");
+        stockBot.readZip("C:\\Users\\Arun\\StockBot\\NASDAQ_2012.zip");
+        System.out.println("Nasdaq 2012 archived");
+//        stockBot.readZip("C:\\Users\\Arun\\StockBot\\NASDAQ_2011.zip");
+//        System.out.println("Nasdaq 2011 archived");
+//        stockBot.readZip("C:\\Users\\Arun\\StockBot\\NASDAQ_2010.zip");
+//        System.out.println("Nasdaq 2010 archived");
+//        stockBot.readZip("C:\\Users\\Arun\\StockBot\\NASDAQ_2009.zip");
+//        System.out.println("Nasdaq 2009 archived");
+
+        final Stock stock = stockBot.stockMap.get("TSLA");
         final StockDate date = StockDate.parse("2014-07-30");
         System.out.println(stock.get(date));
-        
+
         Indicator sma = new SimpleMovingAverage(stock.closingPrices(), 30);
-        System.out.printf("sma=%5g\n",sma.valueAt(date));
-        
+        System.out.printf("sma=%5g\n", sma.valueAt(date));
+
         Indicator ema = new ExponentialMovingAverage(stock.closingPrices(), 30);
-        System.out.printf("ema=%5g\n",ema.valueAt(date));
+        System.out.printf("ema=%5g\n", ema.valueAt(date));
 
         Indicator macd = new MovingAverageConvergenceDivergenceOscillator(stock.closingPrices(), 12, 26, 9);
-        System.out.printf("macd=%5g\n",macd.valueAt(date));
+        System.out.printf("macd=%5g\n", macd.valueAt(date));
+
+        double previousValue = 0;
+        double buyPrice = -1;
+        double totalProfit = 0;
+
+        for (StockDate day : StockDate.range(StockDate.parse("2014-01-29"), StockDate.parse("2014-09-29"))) {
+            final double currentValue = macd.valueAt(day);
+            //System.out.printf("%s: macd=%5g\n", day, currentValue);
+            if (Math.signum(previousValue) != Math.signum(currentValue) && previousValue != 0) {
+                if (currentValue > 0) {
+                    System.out.printf("%s: Buy!\n", day);
+                    buyPrice = stock.closingPrices().valueAt(day);
+                } else {
+                    if (buyPrice != -1) {
+                        double profit = stock.closingPrices().valueAt(day) - buyPrice;
+                        System.out.printf("%s: Sell! Profit = %5g\n", day, profit);
+                        totalProfit += profit;
+
+                    }
+                }
+            }
+            previousValue = currentValue;
+        }
+        System.out.printf("Total Profit = %5g\n", totalProfit);
+
     }
-    private final Map<String,Stock> stockMap = new HashMap<>();
+
+    private final Map<String, Stock> stockMap = new HashMap<>();
 
     private void readZip(String zipFile) throws IOException {
         Path zipPath = Paths.get(zipFile);
@@ -57,7 +95,7 @@ public class StockBot {
                 }
                 String[] tokens = line.split(",");
                 String ticker = tokens[0];
-                StockDate date = StockDate.parse(tokens[1],DateTimeFormatter.BASIC_ISO_DATE);
+                StockDate date = StockDate.parse(tokens[1], DateTimeFormatter.BASIC_ISO_DATE);
                 double open = Double.parseDouble(tokens[2]);
                 double high = Double.parseDouble(tokens[3]);
                 double low = Double.parseDouble(tokens[4]);
