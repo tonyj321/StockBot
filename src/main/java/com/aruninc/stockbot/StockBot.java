aea23a57806087c179faefd2c9b2d4743c3ce57f
arujohn <arujohn@192.168.21.250>
2015-01-07 19:57:12 -0800

    Will now loop over all stocks that exist throughout the data, will only print the profit made on each sale.

--- src/main/java/com/aruninc/stockbot/StockBot.java
+++ src/main/java/com/aruninc/stockbot/StockBot.java
@@ -20,57 +20,76 @@ public class StockBot {
 
     public static void main(String[] args) throws IOException {
         StockBot stockBot = new StockBot();
+        StockBot oldStockBot = new StockBot();
         String home = System.getProperty("user.home");
-        stockBot.readZip(Paths.get(home,"StockBot/NASDAQ_2014.zip"));
+        stockBot.readZip(Paths.get(home, "NetBeansProjects/StockBot/StockBot/NASDAQ_2014.zip"));
         System.out.println("Nasdaq 2014 archived");
-        stockBot.readZip(Paths.get(home,"StockBot/NASDAQ_2013.zip"));
+        stockBot.readZip(Paths.get(home, "NetBeansProjects/StockBot/StockBot/NASDAQ_2013.zip"));
         System.out.println("Nasdaq 2013 archived");
-        stockBot.readZip(Paths.get(home,"StockBot/NASDAQ_2012.zip"));
+        stockBot.readZip(Paths.get(home, "NetBeansProjects/StockBot/StockBot/NASDAQ_2012.zip"));
         System.out.println("Nasdaq 2012 archived");
-        stockBot.readZip(Paths.get(home,"StockBot/NASDAQ_2011.zip"));
+        stockBot.readZip(Paths.get(home, "NetBeansProjects/StockBot/StockBot/NASDAQ_2011.zip"));
         System.out.println("Nasdaq 2011 archived");
-        stockBot.readZip(Paths.get(home,"StockBot/NASDAQ_2010.zip"));
+        stockBot.readZip(Paths.get(home, "NetBeansProjects/StockBot/StockBot/NASDAQ_2010.zip"));
         System.out.println("Nasdaq 2010 archived");
-        stockBot.readZip(Paths.get(home,"StockBot/NASDAQ_2009.zip"));
+        stockBot.readZip(Paths.get(home, "NetBeansProjects/StockBot/StockBot/NASDAQ_2009.zip"));
+        System.out.println("Nasdaq 2009 archived");
+        oldStockBot.readZip(Paths.get(home, "NetBeansProjects/StockBot/StockBot/NASDAQ_2009.zip"));
         System.out.println("Nasdaq 2009 archived");
 
-        final Stock stock = stockBot.stockMap.get("TSLA");
+        Stock stock = stockBot.stockMap.get("IRBT");
         final StockDate date = StockDate.parse("2014-07-30");
         System.out.println(stock.get(date));
-
-        Indicator sma = new SimpleMovingAverage(stock.closingPrices(), 30);
-        System.out.printf("sma=%5g\n", sma.valueAt(date));
-
-        Indicator ema = new ExponentialMovingAverage(stock.closingPrices(), 30);
-        System.out.printf("ema=%5g\n", ema.valueAt(date));
-
-        Indicator macd = new MovingAverageConvergenceDivergenceOscillator(stock.closingPrices(), 12, 26, 9);
-        System.out.printf("macd=%5g\n", macd.valueAt(date));
-
+        
         double previousValue = 0;
         double buyPrice = -1;
+        double percentProfit = 0;
         double totalProfit = 0;
+        double totalPercentProfit = 0;
+        
+        for (Object ticker : oldStockBot.stockMap.keySet()) {
+            stock = stockBot.stockMap.get((String) ticker);
+            try {
+                double subTotalProfit = 0;
+                Indicator sma = new SimpleMovingAverage(stock.closingPrices(), 30);
+                //System.out.printf("sma=%5g\n", sma.valueAt(date));
 
-        for (StockDate day : StockDate.range(StockDate.parse("2014-01-29"), StockDate.parse("2014-09-29"))) {
-            final double currentValue = macd.valueAt(day);
-            //System.out.printf("%s: macd=%5g\n", day, currentValue);
-            if (Math.signum(previousValue) != Math.signum(currentValue) && previousValue != 0) {
-                if (currentValue > 0) {
-                    System.out.printf("%s: Buy!\n", day);
-                    buyPrice = stock.closingPrices().valueAt(day);
-                } else {
-                    if (buyPrice != -1) {
-                        double profit = stock.closingPrices().valueAt(day) - buyPrice;
-                        System.out.printf("%s: Sell! Profit = %5g\n", day, profit);
-                        totalProfit += profit;
+                Indicator ema = new ExponentialMovingAverage(stock.closingPrices(), 30);
+                //System.out.printf("ema=%5g\n", ema.valueAt(date));
 
+                Indicator macd = new MovingAverageConvergenceDivergenceOscillator(stock.closingPrices(), 12, 26, 9);
+                //System.out.printf("macd=%5g\n", macd.valueAt(date));
+
+                
+                for (StockDate day : StockDate.range(StockDate.parse("2014-01-29"), StockDate.parse("2014-09-29"))) {
+
+                    final double currentValue = macd.valueAt(day);
+                    //System.out.printf("%s: macd=%5g\n", day, currentValue);
+                    if (Math.signum(previousValue) != Math.signum(currentValue) && previousValue != 0) {
+                        if (currentValue > 0) {
+                            //System.out.printf("%s: Buy!\n", day);
+                            buyPrice = stock.closingPrices().valueAt(day);
+                        } else {
+                            if (buyPrice != -1) {
+                                double profit = stock.closingPrices().valueAt(day) - buyPrice;
+                                //System.out.printf("%s: Sell! Profit = %5g\n", day, profit);
+                                subTotalProfit += profit;
+
+                            }
+                        }
                     }
+                    previousValue = currentValue;
+
                 }
+                System.out.printf("Sub-Total Profit = %5g\n", subTotalProfit);
+                totalProfit += subTotalProfit;
+                
+            } catch (NullPointerException np) {
+
             }
-            previousValue = currentValue;
+            
         }
         System.out.printf("Total Profit = %5g\n", totalProfit);
-
     }
 
     private final Map<String, Stock> stockMap = new HashMap<>();
